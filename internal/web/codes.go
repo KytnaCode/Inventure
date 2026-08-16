6317e2add987d6483df5cc95259a57994a21d74f
kytnacode <kytnacode@gmail.com>
2026-08-15 19:16:21 -0700

    Document web error codes

--- internal/web/codes.go
+++ internal/web/codes.go
@@ -2,11 +2,20 @@ package web
 
 import "github.com/kytnacode/inventure/pkg/api"
 
+// Code is an API's error code.
 type Code = api.Code
 
+// Error codes.
 var (
-	CodeUserNotFound     = api.NewCode(1)
-	CodeNoPasswordAuth   = api.NewCode(2)
+	// CodeUserNotFound is returned when trying to login as a user who could be found.
+	CodeUserNotFound = api.NewCode(1)
+
+	// CodeNoPasswordAuth is returned when trying to login using a password for a user without
+	// password-based authentication enabled.
+	CodeNoPasswordAuth = api.NewCode(2)
+
+	// CodeWrongCredentials is returned when trying to login as a user but password or email are
+	// wrong.
 	CodeWrongCredentials = api.NewCode(3)
 
 	// CodeMissingCSRFTokenSession is returned if session doesn't contain a valid CSRF token.
