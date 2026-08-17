e47be95888326303f5d0babcf4f6d903ed0397fe
kytnacode <kytnacode@gmail.com>
2026-08-16 23:24:24 -0700

    Rename csrf header

--- internal/auth/csrf/header.go
+++ internal/auth/csrf/header.go
@@ -2,7 +2,7 @@ package csrf
 
 import "net/http"
 
-const HeaderCSRF = "X-CSRF-Token"
+const HeaderCSRF = "X-Csrf-Token"
 
 func FromHeader(r *http.Request) string {
 	return r.Header.Get(HeaderCSRF)
