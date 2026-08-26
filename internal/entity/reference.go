60e3c3bdc0f6c8301699bb01a20edc85260cb94e
kytnacode <kytnacode@gmail.com>
2026-08-26 13:21:59 -0700

    Add reference names

---
+++ internal/entity/reference.go
@@ -0,0 +1,9 @@
+package entity
+
+// Reference are entity reference names for each entity type.
+const (
+	ReferenceItem   = "items"
+	ReferenceRetail = "retails"
+	ReferenceTenant = "tenants"
+	ReferenceUser   = "users"
+)
