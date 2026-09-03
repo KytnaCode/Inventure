bc841a26de8fd377467452d0ae8aa0fb4a1b95a6
kytnacode <kytnacode@gmail.com>
2026-09-03 07:18:10 -0700

    Add test transaction provider

--- transaction/provider.go
+++ transaction/provider.go
@@ -3,36 +3,71 @@ package transaction
 
 import (
 	"context"
+	"sync"
 
 	"gorm.io/gorm"
 )
 
-// Provider allows running multiple operations within a transaction, a custom function is used
-// to construct new repositories or adapters of type T running within a transaction.
+// GormProvider provider allow running multiple repositories inside the same gorm transaction.
 //
 // Safe for concurrent use.
-type Provider[T any] struct {
+type GormProvider[T any] struct {
 	db *gorm.DB
 
 	// constructor function.
 	newFn func(tx *gorm.DB) T
 }
 
-// NewProvider creates a new [Provider] with the given db connection and constructor function. The
-// constructor function must construct required adapters that run its operations within the given
-// transaction.
-func NewProvider[T any](db *gorm.DB, newFn func(tx *gorm.DB) T) *Provider[T] {
-	return &Provider[T]{
+var _ Provider[any] = &TestProvider[any]{}
+
+// TestProvider is a [Provider] meant for testing purposes.
+//
+// Safe for concurrent use.
+type TestProvider[T any] struct {
+	newFn func() T
+	mut   sync.Mutex
+}
+
+// Provider allows running multiple operations within a transaction, a custom function is used
+// to construct new repositories or adapters of type T running within a transaction.
+//
+// Safe for concurrent use.
+type Provider[T any] interface {
+	Transact(ctx context.Context, runFn func(adapters T) error) error
+}
+
+// NewGormProvider creates a new [Provider] with the given db connection and constructor function.
+// The constructor function must construct required adapters that run its operations within the
+// given transaction.
+func NewGormProvider[T any](db *gorm.DB, newFn func(tx *gorm.DB) T) *GormProvider[T] {
+	return &GormProvider[T]{
 		db:    db,
 		newFn: newFn,
 	}
 }
 
+// NewTestProvider creates a new [TestProvider].
+func NewTestProvider[T any](newFn func() T) *TestProvider[T] {
+	return &TestProvider[T]{
+		newFn: newFn,
+	}
+}
+
 // Transact runs a closure within a transaction.
-func (p *Provider[T]) Transact(ctx context.Context, runFn func(adapters T) error) error {
+func (p *GormProvider[T]) Transact(ctx context.Context, runFn func(adapters T) error) error {
 	return p.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		adapters := p.newFn(tx)
 
 		return runFn(adapters)
 	})
+}
+
+// Transact runs `runFn` with the adapters gave to [NewTestProvider], don't run a real transaction.
+func (p *TestProvider[T]) Transact(_ context.Context, runFn func(adapters T) error) error {
+	p.mut.Lock()
+	defer p.mut.Unlock()
+
+	adapters := p.newFn()
+
+	return runFn(adapters)
 }
